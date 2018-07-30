26d835cd5c515e25313fcafe5f730ddfa6d2d1b0
Nguyen Nguyen Hoang <hoang.nguyen@devinition.local>
2018-07-30 17:08:14 +0700

    fix backup.sh

--- database-tools/backup_mysql.sh
+++ database-tools/backup_mysql.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-_container="centos_php:latest"
+_container="max26292/centos_php:latest"
 docker ps -a -q  --filter ancestor=$_container
-cmdOutput=$(docker ps -a -q  --filter ancestor=centos_php:$_container)
+cmdOutput=$(docker ps -a -q  --filter ancestor=$_container)
 __pass="#Hitman5066789"
 __database="test_db"
 echo "Creating backup and restore script for $_container image"
