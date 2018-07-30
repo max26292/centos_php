1038eb6a360a6174a161e899434e1ab8c3c0208e
Nguyen Nguyen Hoang <hoang.nguyen@devinition.local>
2018-07-30 17:30:01 +0700

    update backup sql

--- database-tools/backup_mysql.sh
+++ database-tools/backup_mysql.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
+########## CAUTION please correct the below line as same as the container you wanna backup ####################
 _container="max26292/centos_php:latest"
 docker ps -a -q  --filter ancestor=$_container
 cmdOutput=$(docker ps -a -q  --filter ancestor=$_container)
+########### ROOT PASS #######################
 __pass="#Hitman5066789"
+############# database name #################
 __database="test_db"
 echo "Creating backup and restore script for $_container image"
-exec docker exec -t ${cmdOutput} /usr/bin/mysqldump -u root -p$__pass $__database > backup.sql
+echo "docker exec -t $cmdOutput /usr/bin/mysqldump -u root -p$__pass $__database > backup.sql" > backup.bat
 # echo "docker exec -i ${cmdOutput} /usr/bin/mysql -u root -p$__pass $__database < backup.sql" > restore.bat
 echo "Finished"
-# # Backup
-# docker exec -t ${cmdOutput} /usr/bin/mysqldump -u root -p$__pass $__database > backup.sql
-# # Restore
-# docker exec -i ${cmdOutput} /usr/bin/mysql -u root -p$__pass $__database < backup.sql
--- database-tools/restore_mysql.sh
+++ database-tools/restore_mysql.sh
@@ -4,5 +4,5 @@ docker ps -a -q  --filter ancestor=$_container
 cmdOutput=$(docker ps -a -q  --filter ancestor=$_container)
 __pass="#Hitman5066789"
 __database="test_db"
-echo "docker exec -i ${cmdOutput} /usr/bin/mysql -u root -p$__pass $__database < backup.sql" >restore.bat
+echo "docker exec -i ${cmdOutput} /usr/bin/mysql -u root -p$__pass $__database < backup.sql" > restore.bat
 echo "Finished"
