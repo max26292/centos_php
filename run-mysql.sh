e16193e64a93721751545b9ecd6b35e7328742be
Nguyen Nguyen Hoang <hoang.nguyen@devinition.local>
2018-08-01 13:58:36 +0700

    some fix

--- run-mysql.sh
+++ run-mysql.sh
@@ -10,5 +10,5 @@ echo "SET GLOBAL validate_password_mixed_case_count  = 0 ;" | mysql -uroot -p${M
 echo "SET GLOBAL validate_password_special_char_count = 0 ;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}  
 echo "SHOW VARIABLES LIKE 'validate_password%';"| mysql -uroot -p${MYSQL_ROOT_PASSWORD}
 echo "CREATE DATABASE IF NOT EXISTS  ${MYSQL_DATABASE} ;"| mysql -uroot -p${MYSQL_ROOT_PASSWORD}
-echo "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
+echo "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}' ;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
 echo "GRANT ALL ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' WITH GRANT OPTION ;" | mysql -uroot -p${MYSQL_ROOT_PASSWORD}
